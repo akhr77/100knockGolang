707d259b95e4419c860c8151546d019d5fcfd072
akhr-77 <a.ki.yu.522@gmail.com>
2019-10-17 17:19:07 +0900

    template作成

--- Chap01/05/05.go
+++ Chap01/05/05.go
@@ -1,10 +1,21 @@
 package main
 
+import (
+	"fmt"
+	"strings"
+)
+
 /*
 05. n-gram
 与えられたシーケンス（文字列やリストなど）からn-gramを作る関数を作成せよ．この関数を用い，"I am an NLPer"という文から単語bi-gram，文字bi-gramを得よ．
 */
 
 func main() {
+	line := "I am an NLPer"
+	words := strings.Fields(line)
+
+	for i := 0; len(words)-1 > i; i++ {
+		fmt.Println(words[i], "-", words[i+1])
+	}
 
 }
